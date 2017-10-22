60cc08772ca25d4b5aefd3b665635d9f03a363c7
Mykyta Protsenko <mykyta@protsenko.com>
2017-10-21 20:50:56 -0700

    Zipkin

--- src/main/java/org/grpcvsrest/leaderboard/Application.java
+++ src/main/java/org/grpcvsrest/leaderboard/Application.java
@@ -4,8 +4,10 @@ import io.grpc.Server;
 import io.grpc.netty.NettyServerBuilder;
 import org.grpcvsrest.leaderboard.grpc.LeaderboardGrpcEndpoint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.sleuth.SpanAdjuster;
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
@@ -16,6 +18,11 @@ public class Application {
 
     @Autowired
     private LeaderboardGrpcEndpoint grpcEndpoint;
+
+    @Bean
+    public SpanAdjuster customSpanAdjuster(@Value("${spring.application.name}") String appName) {
+        return span -> span.toBuilder().name("#" + appName + "/" + span.getName().replace("http:/", "")).build();
+    }
 
     @PostConstruct
     public void startGrpcServer() throws IOException {
